506727e113e6077622dc06efa6b679b89a47a753
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2024-05-22 00:36:30 +0100

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
       <div className="container mx-auto flex justify-center items-center">
          <div className="flex flex-col items-center space-y-4">
           <p className="text-xl">Email: elena.barker.cyf@gmail.com</p>
-          <p className="text-xl"> Telephone number: 07498757759</p>
         </div>
       </div>
     </footer>
